b130557a3734deac59c64b54ce0a158d36e55204
01CKZ <108489924+01CKZ@users.noreply.github.com>
2025-06-29 16:28:03 +0800

    Summary

--- lib/views/AppFrame/Summary.dart
+++ lib/views/AppFrame/Summary.dart
@@ -1,10 +1,20 @@
+import 'package:coin_log/constants/MonthMap.dart';
 import 'package:coin_log/main.dart';
+import 'package:coin_log/widgets/ThemedShowMonthPicker.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:pie_chart/pie_chart.dart';
 import 'package:fl_chart/fl_chart.dart' hide PieChart;
 
-class Summary extends StatelessWidget {
+class Summary extends StatefulWidget {
+  @override
+  State<Summary> createState() => _SummaryState();
+}
+
+class _SummaryState extends State<Summary> {
+  String selectedSummaryType = "Monthly";
+  DateTime selectedDateTime = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,25 +31,30 @@ class Summary extends StatelessWidget {
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            SummaryType(),
-            SizedBox(
-              height: 150,
-              child: BalanceSummary()
-            ),
-            Divider(
-              height: 5,
-            ),
-            SizedBox(
-              height: 250,
-              child: TransactionTypeSummary(),
-            ),
-            Divider(
-              height: 5,
-            ),
-            SizedBox(
-              height: 200,
-              width: 350,
-              child: DailyTransactionSummary(),
+            SummaryType(selectedSummaryType: selectedSummaryType, selectedDateTime: selectedDateTime),
+            Expanded(
+              child: ListView(
+                children: [
+                  SizedBox(
+                      height: 100,
+                      child: BalanceSummary(selectedSummaryType: selectedSummaryType)
+                  ),
+                  Divider(
+                    height: 5,
+                  ),
+                  SizedBox(
+                    height: 230,
+                    child: TransactionTypeSummary(),
+                  ),
+                  Divider(
+                    height: 5,
+                  ),
+                  SizedBox(
+                    height: 250,
+                    child: DailyTransactionSummary(),
+                  )
+                ],
+              ),
             )
           ],
         ),
@@ -48,46 +63,81 @@ class Summary extends StatelessWidget {
   }
 }
 
-class SummaryType extends StatelessWidget {
+class SummaryType extends StatefulWidget {
+  String selectedSummaryType;
+  DateTime selectedDateTime;
+
+  SummaryType({
+    required this.selectedSummaryType,
+    required this.selectedDateTime
+  });
+
+  @override
+  State<SummaryType> createState() => _SummaryTypeState();
+}
+
+class _SummaryTypeState extends State<SummaryType> {
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
+    return Column(
       children: [
-        InkWell(
-          onTap: () {
-            //
-          },
-          child: Container(
-            padding: EdgeInsetsDirectional.fromSTEB(0, 5, 0, 5),
-            width: MediaQuery.of(context).size.width * 0.4,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              border: Border(
-                bottom: BorderSide(width: 2)
-              )
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            InkWell(
+              onTap: () {
+                setState(() {
+                  widget.selectedSummaryType = "Monthly";
+                });
+              },
+              child: Container(
+                padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                width: MediaQuery.of(context).size.width * 0.4,
+                alignment: Alignment.center,
+                child: Text("Monthly",
+                    style: TextStyle(
+                      fontWeight: widget.selectedSummaryType == "Monthly" ? FontWeight.bold : FontWeight.normal
+                    ),
+                  ),
+              ),
             ),
-            child: Text("Monthly",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold
+            InkWell(
+              onTap: () {
+                setState(() {
+                  widget.selectedSummaryType = "Yearly";
+                });
+              },
+              child: Container(
+                padding: EdgeInsetsDirectional.fromSTEB(0, 5, 0, 5),
+                width: MediaQuery.of(context).size.width * 0.4,
+                alignment: Alignment.center,
+                child: Text("Yearly",
+                  style: TextStyle(
+                      fontWeight: widget.selectedSummaryType == "Yearly" ? FontWeight.bold : FontWeight.normal
+                  ),
                 ),
               ),
-          ),
-        ),
-        InkWell(
-          onTap: () {
-            //
-          },
-          child: Container(
-            padding: EdgeInsetsDirectional.fromSTEB(0, 5, 0, 5),
-            width: MediaQuery.of(context).size.width * 0.4,
-            alignment: Alignment.center,
-            child: Text("Yearly",
-              style: TextStyle(
-                  fontWeight: FontWeight.normal
-              ),
             ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+          child: Row(
+            children: [
+              InkWell(
+                onTap: () async {
+                  DateTime? tempDateTime = await themedShowMonthPicker(context, widget.selectedDateTime);
+                  if (tempDateTime != null) {
+                    setState(() {
+                      widget.selectedDateTime = tempDateTime;
+                    });
+                  }
+                },
+                child: Text("${monthMap[widget.selectedDateTime.month.toString()]!} ${widget.selectedDateTime.year}"),
+              )
+            ],
           ),
         ),
       ],
@@ -95,31 +145,29 @@ class SummaryType extends StatelessWidget {
   }
 }
 
-class BalanceSummary extends StatelessWidget {
+class BalanceSummary extends StatefulWidget {
+  final String selectedSummaryType;
+
+  BalanceSummary({
+    required this.selectedSummaryType
+  });
+
+  @override
+  State<BalanceSummary> createState() => _BalanceSummaryState();
+}
+
+class _BalanceSummaryState extends State<BalanceSummary> {
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        InkWell(
-          onTap: () {
-            //
-          },
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-            child: Row(
-              children: [
-                Text("June 2025 ")
-              ],
-            ),
-          ),
-        ),
         Padding(
           padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
           child: Stack(
             alignment: AlignmentDirectional.topStart,
             children: [
-              Container(height: 35, width: double.infinity, color: Theme.of(context).colorScheme.success),
-              Container(height: 35, width: 150, color: Theme.of(context).colorScheme.error)
+              Container(height: 25, width: double.infinity, color: Theme.of(context).colorScheme.success),
+              Container(height: 25, width: 150, color: Theme.of(context).colorScheme.error)
             ],
           ),
         ),
@@ -230,18 +278,21 @@ class TransactionTypeSummary extends StatelessWidget {
       child: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+            padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
             child: Row(
               children: [
                 Text("Expenses")
               ],
             ),
           ),
-          PieChart(
-            dataMap: dataMap,
-            colorList: [Colors.red, Colors.green, Colors.blue],
-            chartRadius: 150,
-            chartType: ChartType.ring
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+            child: PieChart(
+              dataMap: dataMap,
+              colorList: [Colors.red, Colors.green, Colors.blue],
+              chartRadius: 150,
+              chartType: ChartType.ring
+            ),
           )
         ],
       ),
@@ -253,63 +304,76 @@ class TransactionTypeSummary extends StatelessWidget {
 class DailyTransactionSummary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-      child: LineChart(
-        LineChartData(
-          minX: 1,
-          maxX: 7, // 7 days
-          minY: 0,
-          maxY: 1000,
-          titlesData: FlTitlesData(
-            bottomTitles: AxisTitles(
-              sideTitles: SideTitles(
-                showTitles: true,
-                interval: 1,
-                getTitlesWidget: (value, meta) {
-                  return Text('${value.toInt()}', style: const TextStyle(fontSize: 11));
-                },
-              ),
-            ),
-            leftTitles: AxisTitles(
-              sideTitles: SideTitles(
-                showTitles: true,
-                interval: 300,
-                getTitlesWidget: (value, meta) {
-                  final formattedValue = NumberFormat.compact().format(value);
-                  return Text(formattedValue, style: const TextStyle(fontSize: 11));
-                },
-              ),
-            ),
-            topTitles: AxisTitles(
-              sideTitles: SideTitles(showTitles: false), // 🔴 hide top axis
-            ),
-            rightTitles: AxisTitles(
-              sideTitles: SideTitles(showTitles: false), // 🔴 hide right axis
-            ),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+          child: Row(
+            children: [
+              Text("Daily Expenses")
+            ],
           ),
+        ),
+        Container(
+          padding: const EdgeInsets.fromLTRB(0, 15, 0, 0),
+          height: 200,
+          child: LineChart(
+            LineChartData(
+              minX: 1,
+              maxX: 7, // 7 days
+              minY: 0,
+              maxY: 1000,
+              titlesData: FlTitlesData(
+                bottomTitles: AxisTitles(
+                  sideTitles: SideTitles(
+                    showTitles: true,
+                    interval: 1,
+                    getTitlesWidget: (value, meta) {
+                      return Text('${value.toInt()}', style: const TextStyle(fontSize: 11));
+                    },
+                  ),
+                ),
+                leftTitles: AxisTitles(
+                  sideTitles: SideTitles(
+                    showTitles: true,
+                    interval: 300,
+                    getTitlesWidget: (value, meta) {
+                      final formattedValue = NumberFormat.compact().format(value);
+                      return Text(formattedValue, style: const TextStyle(fontSize: 11));
+                    },
+                  ),
+                ),
+                topTitles: AxisTitles(
+                  sideTitles: SideTitles(showTitles: false), // 🔴 hide top axis
+                ),
+                rightTitles: AxisTitles(
+                  sideTitles: SideTitles(showTitles: false), // 🔴 hide right axis
+                ),
+              ),
 
-          gridData: FlGridData(show: true),
-          borderData: FlBorderData(show: true),
+              gridData: FlGridData(show: true),
+              borderData: FlBorderData(show: true),
 
-          lineBarsData: [
-            LineChartBarData(
-              barWidth: 3,
-              color: Colors.blue,
-              dotData: FlDotData(show: true),
-              spots: [
-                FlSpot(1, 50), // Day 1: $50
-                FlSpot(2, 30), // Day 2: $30
-                FlSpot(3, 70), // Day 3: $70
-                FlSpot(4, 20), // ...
-                FlSpot(5, 90),
-                FlSpot(6, 60),
-                FlSpot(7, 80),
+              lineBarsData: [
+                LineChartBarData(
+                  barWidth: 3,
+                  color: Colors.blue,
+                  dotData: FlDotData(show: true),
+                  spots: [
+                    FlSpot(1, 50), // Day 1: $50
+                    FlSpot(2, 30), // Day 2: $30
+                    FlSpot(3, 70), // Day 3: $70
+                    FlSpot(4, 20), // ...
+                    FlSpot(5, 90),
+                    FlSpot(6, 60),
+                    FlSpot(7, 80),
+                  ],
+                ),
               ],
             ),
-          ],
+          ),
         ),
-      ),
+      ],
     );
   }
 
